6e98cc169a360723c272b955b9aebbd96b56921b
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-02 20:09:39 +0700

    Add files via upload

---
+++ Ex03_PalindromeNumber/Ex03_PalindromeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex03_PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            Console.WriteLine(solution.IsPalindrome(12231));
+        }
+    }
+}
---
+++ Ex03_PalindromeNumber/Ex03_PalindromeNumber/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int number)
+        {
+            int result, temp;
+            result = 0;
+            temp = number;
+
+            while (temp > 0)
+            {
+                result = result * 10 + temp % 10;
+                temp = temp / 10;
+            }
+
+            return result == number;
+
+        }
+    }
+}
